ef051b7194599ba39577098c5842a6c208d7390a
arsal uddin <arsaluddin134@gmail.com>
2023-08-12 12:42:58 +0530

    error-issue

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-const express = require("express")
+const express = require("express");
+const cors = require("cors"); // Import the cors middleware
 const port = process.env.PORT || 5000;
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const formRoutes = require('./routes/formTemplates');
 
-app.use(bodyParser.json())
+app.use(cors()); // Use the cors middleware
+app.use(bodyParser.json());
 app.use(express.json());
-app.use('/form', formRoutes);
+app.use('/forms', formRoutes);
 
-app.listen(port , () => {
-    console.log("listening on port "+`${port}`);
-})
+app.listen(port, () => {
+    console.log("listening on port " + `${port}`);
+});
