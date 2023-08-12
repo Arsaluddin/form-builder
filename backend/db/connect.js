a1f191daee21ead411025b5f8e02020e0f686687
arsal uddin <arsaluddin134@gmail.com>
2023-08-12 16:33:48 +0530

    security

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+
 // key --> tFH50Qyb33Q7PVtw 
 
-mongoose.connect('mongodb+srv://arsaluddin134:tFH50Qyb33Q7PVtw@cluster0.qrdtcpl.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+// db.on('error', console.error.bind(console, 'Connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectDB;
